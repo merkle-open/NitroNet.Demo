7fef1ba0b38f1a92ee930aea7a57bddf99a005db
Amir Begic <amir.begic@namics.com>
2018-01-12 13:59:22 +0100

    Activated Nitro config and created campatible frontend filestructure with handlebar files.

--- NitroNet.Demo/Controllers/HomeController.cs
+++ NitroNet.Demo/Controllers/HomeController.cs
@@ -10,7 +10,12 @@ namespace NitroNet.Demo.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var model = new
+            {
+                GettinStarted =
+                "ASP.NET MVC gives you a powerful, patterns-based way to build dynamic websites that enables a clean separation of concerns and gives you full control over markup for enjoyable, agile development."
+            };
+            return View("frontend/patterns/molecules/index/index", model);
         }
 
         public ActionResult About()
